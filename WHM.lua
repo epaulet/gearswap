456e6bf6e5a0360f803b72e51a4fb5f0da18cf2e
Edward Paulet <espaulet@gmail.com>
2019-03-21 22:23:30 -0700

    Update WHM.lua
    Add Sors Shield and Gendewitha Bliaut

--- WHM.lua
+++ WHM.lua
@@ -3,10 +3,10 @@ require 'helpers'
 function get_sets()
 	sets.Idle = {
 		main="Homestead Wand",
-		sub="Hoplon",
+		sub="Sors Shield",
 		ammo="Homiliary",
 		head="Weath. Corona +1",
-		body="Weather. Robe +1",
+		body="Gendewitha Bliaut",
 		hands="Weath. Cuffs +1",
 		legs="Weath. Pants +1",
 		feet="Weath. Souliers +1",
@@ -16,22 +16,26 @@ function get_sets()
 		right_ear="Spellbreaker Earring",
 		left_ring="Perception Ring",
 		right_ring="Chrysoberyl Ring",
-		back="Dew Silk Cape +1",
+		back="Pahtli Cape",
 	}
 
 	sets.Magic = {}
 	sets.Magic.Precast = {}
 	sets.Magic.Precast.Base = set_combine(sets.Idle, {})
-	sets.Magic.Precast.Cure = set_combine(sets.Magic.Precast.Base, {
-		back="Pahtli Cape",
-	})
+
+	-- TODO: Differentiate offensive and defensive Cure
+	sets.Magic.Precast.Cure = set_combine(sets.Magic.Precast.Base, {})
 
 	sets.Magic.Midcast = {}
 	sets.Magic.Midcast.Base = set_combine(sets.Idle, {
 		head="Telchine Cap",
+		body="Weather. Robe +1",
+		back="Dew Silk Cape +1",
 	})
+	-- TODO: Single target vs AOE heals once I have healing vs MND gear
 	sets.Magic.Midcast.Cure = set_combine(sets.Magic.Midcast.Base, {
 		head="Marduk's Tiara +1",
+		body="Gendewitha Bliaut",
 	})
 	sets.Magic.Midcast.Regen = set_combine(sets.Magic.Midcast.Base, {
 		head="Marduk's Tiara +1",
@@ -42,11 +46,13 @@ function get_sets()
 	-- TODO: WS set
 	sets.TP = set_combine(sets.Idle, {
 		head="Telchine Cap",
-		neck="Focus Collar", 
+		neck="Focus Collar",
+		body="Weather. Robe +1",
 		left_ear="Fang Earring",
 		right_ear="Tortoise Earring",
 		left_ring="Oneiros Annulet",
 		right_ring="Enlivened Ring",
+		back="Dew Silk Cape +1"
 	})
 end
 
